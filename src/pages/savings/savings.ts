d80d54c29adbbe3c7795e7c9edb2c8e753fa7231
radityasurya <radityasurya@hotmail.com>
2017-07-25 15:58:20 +0200

    Update fixes

--- src/pages/savings/savings.ts
+++ src/pages/savings/savings.ts
@@ -1,5 +1,5 @@
 import { TargetPage } from './../target/target';
-import { Component } from '@angular/core';
+import {Component, Input, ViewChild} from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -8,9 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 
 export class SavingsPage {
+  @ViewChild('input') savingInput ;
+
+  money: number = 56.500;
   constructor(public navCtrl: NavController) {
 
   }
+
+  ionViewLoaded() {
+
+    setTimeout(() => {
+      this.savingInput.setFocus();
+    },150);
+
+ }
+
 
   next() {
     this.navCtrl.push(TargetPage);
